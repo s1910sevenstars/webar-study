1f478856548a7bb6041abf042e892103558c1ebc
i.sugawara <sugawara@nextribe.co.jp>
2019-10-08 17:14:37 +0900

    Updates

---
+++ precache-manifest.18f62bb211ad0fc9fe1abcb89295c8ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6bc242ddc546c146f3e5be48c3e123",
+    "url": "/webar-study/index.html"
+  },
+  {
+    "revision": "b05e982732742381fdab",
+    "url": "/webar-study/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4bd60c849cef0a5aa120",
+    "url": "/webar-study/static/js/2.67bfb5c5.chunk.js"
+  },
+  {
+    "revision": "b05e982732742381fdab",
+    "url": "/webar-study/static/js/main.23df134c.chunk.js"
+  },
+  {
+    "revision": "fb14c4bdbb900b5de6f7",
+    "url": "/webar-study/static/js/runtime-main.1feb7ecd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/webar-study/static/media/logo.25bf045c.svg"
+  }
+]);
